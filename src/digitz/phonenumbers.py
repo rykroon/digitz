d8133c423bb105541b8186d307d449b894b5755f
Ryan Kroon <rykroon.tech@gmail.com>
2024-12-14 10:30:50 -0500

    Added docstrings for all methods.

--- src/digitz/phonenumbers.py
+++ src/digitz/phonenumbers.py
@@ -6,6 +6,8 @@ from functools import lru_cache, cached_property
 from typing import Optional, Tuple, Type, TypeVar, Union
 
 import phonenumbers as pn
+import pytz
+from pytz.tzinfo import BaseTzInfo
 
 from digitz.enums import (
     CountryCodeSource,
@@ -28,6 +30,8 @@ Self = TypeVar("Self", bound="PhoneNumber")
 
 @dataclass(frozen=True)
 class PhoneNumber(pn.PhoneNumber):
+    """A dataclass representing a phone number."""
+
     country_code: int
     national_number: int
     extension: Optional[str] = None
@@ -46,6 +50,7 @@ class PhoneNumber(pn.PhoneNumber):
         region: Optional[str] = None,
         keep_raw_input: bool = False,
     ) -> Self:
+        """Parse a phone number."""
         try:
             numobj = pn.parse(number, region=region, keep_raw_input=keep_raw_input)
 
@@ -68,13 +73,13 @@ class PhoneNumber(pn.PhoneNumber):
             else:
                 raise e  # pragma: no cover
 
-        # country_code and national_number should never be None.
-        # These checks are for type checking purposes.
+        # Set default values for country_code and national_number
+        # to avoid type checking errors
         if numobj.country_code is None:
-            numobj.country_code = 0
+            numobj.country_code = 0  # pragma: no cover
 
         if numobj.national_number is None:
-            numobj.national_number = 0
+            numobj.national_number = 0  # pragma: no cover
 
         return cls(
             country_code=numobj.country_code,
@@ -88,74 +93,93 @@ class PhoneNumber(pn.PhoneNumber):
         )
 
     def __str__(self) -> str:
+        """Return the E.164 representation of the phone number."""
         return self.to_e164()
 
     @cached_property
     def national_destination_code_length(self) -> int:
+        """Return the length of the national destination code."""
         return pn.length_of_national_destination_code(self)
 
     @cached_property
     def national_significant_number(self) -> str:
+        """Return the national significant number."""
         return pn.national_significant_number(self)
 
     @property
     def national_destination_code(self) -> str:
+        """Return the national destination code."""
         return self.national_significant_number[: self.national_destination_code_length]
 
     @property
     def subscriber_number(self) -> str:
+        """Return the subscriber number."""
         return self.national_significant_number[self.national_destination_code_length :]
 
     @cached_property
     def number_type(self) -> PhoneNumberType:
+        """Return the type of the phone number."""
         return PhoneNumberType(pn.number_type(self))
 
     @cached_property
     def region_code(self) -> Optional[str]:
+        """Return the region code of the phone number."""
         return pn.region_code_for_number(self)
 
     @cached_property
-    def timezones(self) -> Tuple[str, ...]:
+    def timezones(self) -> Tuple[BaseTzInfo, ...]:
+        """Return the timezones of the phone number."""
         from phonenumbers.timezone import time_zones_for_number
 
-        return time_zones_for_number(self)
+        return tuple([pytz.timezone(zone) for zone in time_zones_for_number(self)])
 
     @cached_property
     def is_geographical(self) -> bool:
+        """Return whether the phone number is geographical."""
         return pn.is_number_geographical(self)
 
     @cached_property
     def is_possible(self) -> bool:
+        """Return whether the phone number is possible."""
         return pn.is_possible_number(self)
 
     @cached_property
     def is_valid(self) -> bool:
+        """Return whether the phone number is valid."""
         return pn.is_valid_number(self)
 
     @property
     def is_toll_free(self) -> bool:
+        """Return whether the phone number is toll-free."""
         return self.number_type == PhoneNumberType.TOLL_FREE
 
     @property
     def is_voip(self) -> bool:
+        """Return whether the phone number is VoIP."""
         return self.number_type == PhoneNumberType.VOIP
 
     def match(self, other: Union[str, pn.PhoneNumber], /) -> MatchType:
+        """Return the match type of the phone number."""
         return MatchType(pn.is_number_match(self, other))
 
     def is_no_match(self, other: Union[str, pn.PhoneNumber], /) -> bool:
+        """Return whether the phone number is no match."""
         return self.match(other) == MatchType.NO_MATCH
 
     def is_short_nsn_match(self, other: Union[str, pn.PhoneNumber], /) -> bool:
+        """Return whether the phone number is a short NSN match."""
         return self.match(other) == MatchType.SHORT_NSN_MATCH
 
     def is_nsn_match(self, other: Union[str, pn.PhoneNumber], /) -> bool:
+        """Return whether the phone number is a NSN match."""
         return self.match(other) == MatchType.NSN_MATCH
 
     def is_exact_match(self, other: Union[str, pn.PhoneNumber], /) -> bool:
+        """Return whether the phone number is an exact match."""
         return self.match(other) == MatchType.EXACT_MATCH
 
     def is_any_match(self, other: Union[str, pn.PhoneNumber], /) -> bool:
+        """Return whether the phone number is any match."""
         return (
             self.is_exact_match(other)
             or self.is_nsn_match(other)
@@ -164,36 +188,44 @@ class PhoneNumber(pn.PhoneNumber):
 
     @lru_cache
     def get_carrier_name(self, lang: str) -> str:
+        """Return the carrier name of the phone number."""
         from phonenumbers.carrier import name_for_number
 
         return name_for_number(self, lang=lang)
 
     @lru_cache
     def get_country_name(self, lang: str) -> str:
+        """Return the country name of the phone number."""
         from phonenumbers.geocoder import country_name_for_number
 
         return country_name_for_number(self, lang=lang)
 
     @lru_cache
     def get_description(self, lang: str) -> str:
+        """Return the description of the phone number."""
         from phonenumbers.geocoder import description_for_number
 
         return description_for_number(self, lang=lang)
 
     @lru_cache
     def format(self, format: PhoneNumberFormat) -> str:
+        """Return the formatted phone number."""
         return pn.format_number(self, format)
 
     def to_e164(self) -> str:
+        """Return the E.164 representation of the phone number."""
         return self.format(PhoneNumberFormat.E164)
 
     def to_international(self) -> str:
+        """Return the international representation of the phone number."""
         return self.format(PhoneNumberFormat.INTERNATIONAL)
 
     def to_national(self) -> str:
+        """Return the national representation of the phone number."""
         return self.format(PhoneNumberFormat.NATIONAL)
 
     def to_rfc3966(self) -> str:
+        """Return the RFC3966 representation of the phone number."""
         return self.format(PhoneNumberFormat.RFC3966)
 
     def replace(
@@ -205,6 +237,7 @@ class PhoneNumber(pn.PhoneNumber):
         italian_leading_zero: Optional[bool] = None,
         number_of_leading_zeros: Optional[int] = None,
     ) -> Self:
+        """Return a new phone number with the specified attributes replaced."""
         if country_code is None:
             country_code = self.country_code
 
