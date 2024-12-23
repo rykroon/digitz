ddee5312ff63974622ca7f4f628e88d8eaa484d2
Ryan Kroon <rykroon.tech@gmail.com>
2024-12-22 22:32:42 -0500

    Removed error_type as class variable.

--- src/digitz/exceptions.py
+++ src/digitz/exceptions.py
@@ -15,28 +15,22 @@ __all__ = [
 class InvalidCountryCode(NumberParseException):
     """Raised when the country code is invalid."""
 
-    error_type = NumberParseErrorType.INVALID_COUNTRY_CODE
-
     def __init__(self, msg: str):
-        super().__init__(self.error_type, msg)
+        super().__init__(NumberParseErrorType.INVALID_COUNTRY_CODE, msg)
 
 
 class NotANumber(NumberParseException):
     """Raised when the string is not a number."""
 
-    error_type = NumberParseErrorType.NOT_A_NUMBER
-
     def __init__(self, msg: str):
-        super().__init__(self.error_type, msg)
+        super().__init__(NumberParseErrorType.NOT_A_NUMBER, msg)
 
 
 class TooLong(NumberParseException):
     """Raised when the number is too long."""
 
-    error_type = NumberParseErrorType.TOO_LONG
-
     def __init__(self, msg: str):
-        super().__init__(self.error_type, msg)
+        super().__init__(NumberParseErrorType.TOO_LONG, msg)
 
 
 class TooShort(NumberParseException):
@@ -46,16 +40,12 @@ class TooShort(NumberParseException):
 class TooShortAfterIDD(TooShort):
     """Raised when the number is too short after International Direct Dialing (IDD)."""
 
-    error_type = NumberParseErrorType.TOO_SHORT_AFTER_IDD
-
     def __init__(self, msg: str):
-        super().__init__(self.error_type, msg)
+        super().__init__(NumberParseErrorType.TOO_SHORT_AFTER_IDD, msg)
 
 
 class TooShortNsn(TooShort):
     """Raised when the number is shorter than the National Significant Number (NSN)."""
 
-    error_type = NumberParseErrorType.TOO_SHORT_NSN
-
     def __init__(self, msg: str):
-        super().__init__(self.error_type, msg)
+        super().__init__(NumberParseErrorType.TOO_SHORT_NSN, msg)
